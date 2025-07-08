abdaf17d9b713bc9486d16d5b88924e88a82f725
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-08 17:29:20 +0545

    Implement defaultGreet function to return personalized greetings based on provided names

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+//My code is given below
+function defaultGreet(firstName, lastName = 'Doe') {
+    if (lastName !== 'Doe') {
+        return `Hi ${firstName} ${lastName}!`;
+    } else {
+        return `Hi ${firstName} Doe!`;
+    }
+}
+defaultGreet("Ramesh","Pariyar");
