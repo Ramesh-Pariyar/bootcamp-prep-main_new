8b0975c9c12b04f3d0a6cebd7f4a3d34ad1a3374
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-08 18:54:16 +0545

    Implement myMnemonic function to generate mnemonic from provided strings

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function myMnemonic(str1="",str2="",str3="",str4=""){
+    if (str1 == "" ){
+        return "string"
+    }
+    else if (str2 == "" && str3 =="" && str4 == ""){
+        return str1[0].toUpperCase();
+    }
+    else if (str3 == "" && str4 ==""){
+        return (str1[0]+str2[0]).toUpperCase();
+
+    }
+    else if ( str4 ==""){
+        return (str1[0]+str2[0]+str3[0]).toUpperCase();
+    }
+    else{
+        return (str1[0]+str2[0]+str3[0]+str4[0]).toUpperCase();
+    }
+}
