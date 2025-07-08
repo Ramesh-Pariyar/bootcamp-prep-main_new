cb3215b3b62a4089d0617be48b3d256f4f56981c
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-08 17:12:48 +0545

    Add simpleSum function to calculate and log the sum of two numbers

--- pre-work-workshops/05-functions/01-simple-sum/simple-sum.js
+++ pre-work-workshops/05-functions/01-simple-sum/simple-sum.js
@@ -1,1 +1,7 @@
-// YOUR CODE BELOW
+// My code is here
+let sum = 0;
+function simpleSum(num1,num2) {
+    sum = num1 + num2;
+    return sum;
+}
+console.log("the sum of two numbers are :", simpleSum(2,3));
