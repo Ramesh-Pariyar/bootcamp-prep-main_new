43bed05623478eb993413d04a42d6eeb95ea7f4b
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-08 18:54:38 +0545

    Implement doYouPlayTheTheremin function to check if the input starts with 'S' or 's'

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,8 @@
-// YOUR CODE BELOW
+//here is my code
+function doYouPlayTheTheremin (str){
+    if (str[0] === "S" || str[0]=== "s"){
+        return true;
+    }else{
+        return false;
+    }
+}
