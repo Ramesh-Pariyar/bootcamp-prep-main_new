44b1bc5ec38ce3a1b91ebcb98095cc272855e058
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 14:12:04 +0545

    Implement exponentiation function with handling for edge cases

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,14 @@
-// YOUR CODE BELOW
+// here is my code
+function exponentiate(base,power){
+    let result = 0;
+    if (power == 1){
+        result = 1;
+    }
+    else if (base==0 && power ==0){
+        result = "undefined"
+    }else{
+        result = base ** power;
+    }
+    return result;
+}
+console.log(exponentiate(2,4))
