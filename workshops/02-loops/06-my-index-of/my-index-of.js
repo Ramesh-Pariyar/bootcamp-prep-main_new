287f2d516b433538b0d066e438f80536182ff8e7
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 16:56:22 +0545

    Implement mySlice function to extract a substring from a given string

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,12 @@
-// YOUR CODE BELOW
+//Here is my code
+debugger;
+function mySlice(originalString, startIdx = 0 ,endIdx=originalString.length){
+     
+    let slicedString = ""
+
+    for (let i = startIdx; i< endIdx; i++ ){
+        slicedString += originalString[i]
+    }
+    return slicedString
+}
+console.log(mySlice("slice and dice",3,8))
