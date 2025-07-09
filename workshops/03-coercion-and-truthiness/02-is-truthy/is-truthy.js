b56df46398b9e2a3ad6e0631d8a0110b7782d3da
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 18:39:10 +0545

    Add detailed comments and examples to isTruthy function for clarity

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,43 @@
-// YOUR CODE BELOW
+// // Here is my code
+
+
+// Write a function `isTruthy` that accepts a single argument of any type.
+
+// `isTruthy` should return true if that argument is 'truthy'.
+
+// If the value is 'falsey', log out one of the messages below, corresponding to
+// the type of the value tested.
+
+// ```
+// 'The boolean value false is falsey'
+// 'The null value is falsey'
+// 'undefined is falsey'
+// 'The number 0 is falsey (the only falsey number)'
+// 'The empty string is falsey (the only falsey string)'
+// ```
+
+// ```javascript
+// isTruthy('I yearn for truth'); // => true
+// isTruthy(null); // => The null value is falsey
+// ```
+function isTruthy(value){
+    if(!!value){
+        return true
+    }
+    if (value===false){
+        return "The boolean value false is falsey"
+    }
+    if (value===null){
+        return "The null value is falsey"
+    }
+    if (value===undefined){
+        return "undefined is falsey"
+    }
+    if (value===0){
+        return 'The number 0 is falsey (the only falsey number)'
+    }
+    if(value ===""){
+        return 'The empty string is falsey (the only falsey string)'
+    }
+}
+
