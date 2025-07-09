8c11908b0e8577a9a34ba75bb5b8220b6a547a4f
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 19:52:14 +0545

    Add zeroDarkThirty function to remove zeros from number input

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,18 @@
-// YOUR CODE BELOW
+//here is my code
+function zeroDarkThirty(aNumber){
+    if (!aNumber){
+        return NaN
+    }
+    let strVal = String(aNumber);
+    let newVal = ''
+    for(i=0; i<strVal.length;i++){
+
+        if(strVal[i]==='0'){
+                continue
+        }
+        newVal += strVal[i]
+    }
+    return Number(newVal)
+
+}
+console.log(zeroDarkThirty(12000122))
