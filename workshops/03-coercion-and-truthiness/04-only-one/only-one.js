59109dce32b6f0ab090a8f0a97d17a9e8f072aae
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 19:10:58 +0545

    Refactor onlyOne function for clarity and improved logic

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,17 @@
-// YOUR CODE BELOW
+// Here is my code
+function onlyOne(a,b,c){
+    let count = 0;
+    if (a){
+        count++;
+    }
+    if (b) count++;
+    if (c)count++;
+    
+    if(count!=1){
+        return false;
+    }
+    else{
+        return true;
+    }
+        
+}
