68890b8b44abc9e6cdafbc57d2a0142b3a24a425
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 19:00:50 +0545

    Implement mostVowels and myOr/myAnd functions with initial logic

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,11 @@
-// YOUR CODE BELOW
+//my code
+function mostVowels (str){
+    let max =0
+    for (i=0; i<str.length;i++){
+        for (let j=0;j)
+        count = 0;
+        if (str[0]==='a'||str[0]==='e'||str[0]==='i'||str[0]==='o'||str[0]==='u'){
+        count+=1;
+    }
+    }
+}
--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,28 @@
-// YOUR CODE BELOW
+// here is my code
+function myOr(a,b,c){
+    if (a){
+        return a
+    }
+    else if(b){
+        return b
+    }
+    else {
+
+        return c
+    }
+    
+}
+function myAnd(a,b,c){
+    if (!a){
+        return a
+    }
+    else if (!b){
+        return b
+    }
+    else{
+        return c
+    }
+
+}
+console.log(myOr(1))
+console.log(myAnd(2,'t',true))
