62b8baa2928d13734d2686c4972c887ba69f65dc
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 13:04:30 +0545

    Add tempCodeRunnerFile.js for only odds workshop

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,14 @@
-// YOUR CODE BELOW
+// HERE IS MY CODE
+function onlyOdds(number){
+    debugger;
+    let sum = 0;
+    if(number<0) return 0;
+
+    for (let i = 1; i<number; i++){
+            if (i%2===1){
+                sum= sum +i;
+            }
+    }
+        return sum;
+}
+console.log(onlyOdds(6));
