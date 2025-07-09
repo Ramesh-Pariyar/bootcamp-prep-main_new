733db4f133f4c0a35262d9fb167466c520953821
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 13:33:41 +0545

    Refactor crazyCaps function for improved clarity and structure

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,20 @@
-// YOUR CODE BELOW
+//MY CODE IS HERE
+function crazyCaps(strVal){
+    let output = '';
+    for(let i=0; i<strVal.length; i++){
+        if (i===0){
+            output = output + strVal[0].toLowerCase();
+        }
+        else {
+
+            if (i%2==1){
+                output = output + strVal[i].toUpperCase();
+            }
+            else{
+                output = output + strVal[i]
+            }
+
+        }
+    }
+return output;
+}
