79df574bd5b91291a86ca1ddbaf6804e6db3de9e
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 20:33:23 +0545

    Refactor incrementalChange function to improve structure and add console logs for testing

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,9 @@
 let counter = 0;
 
-
+function incrementalChange (num){
+    counter +=num;
+    return counter;
+}
+console.log(incrementalChange(2))
+console.log(incrementalChange(6))
+console.log(incrementalChange(-1))
