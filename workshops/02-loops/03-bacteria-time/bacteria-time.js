8874d7595572ef6baccbad4c5cf1036b456dee95
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-09 14:05:52 +0545

    bacteria time

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,13 @@
-// YOUR CODE BELOW
+//Here is my 
+console.log('ramesh');
+function bacteriaTime(currentNum,targetNum){
+    let reqTime = 0;
+    if (currentNum>targetNum){
+        return 'targetNum must be larger than currentNum'
+    }
+    for (let i=currentNum; i<targetNum; i*=2 ){
+        reqTime +=20;
+    }
+    return reqTime;
+}
+console.log(bacteriaTime(200,3200));        
