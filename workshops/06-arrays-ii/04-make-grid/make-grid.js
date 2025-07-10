011bf32e8bfdda0d688987a2be6f5893aa163189
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 18:13:34 +0545

    making grid

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,32 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+
+// Write a function `makeGrid` that accepts two arguments:
+// 1. numColumns (number)
+// 2. numRows (number)
+
+// `makeGrid` should return a two-dimensional array that represents a grid of the
+// given dimensions.
+
+// ```javascript
+// makeGrid(3,4);
+// /* => [[1, 2, 3],
+//        [1, 2, 3],
+//        [1, 2, 3],
+//        [1, 2, 3]]
+// */
+
+function makeGrid (numColumns, numRows){
+
+    let finalGrid = [];
+
+    for (i = 0 ; i < numRows; i++){
+        let semiFinalGrid = [];
+
+        for (j= 0; j<numColumns; j++){
+            semiFinalGrid.push(j+1)
+        }
+
+        finalGrid.push(semiFinalGrid)
+    }
+    return finalGrid;
+}
