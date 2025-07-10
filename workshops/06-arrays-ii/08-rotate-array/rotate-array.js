12ddb8261f267a2e5efbf4c000e68cadaaa082ad
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 20:51:40 +0545

    rotation of arrray by using slice

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+// here is my code
+
+function rotateArray(originalArry,rotationNum){
+
+    let front = originalArry.slice (-rotationNum)
+    let back = originalArry.slice(0,-rotationNum)
+    finalRotatedArray = front.concat(back)
+    return finalRotatedArray;
+}
