a67233b2c425e923d8cd44d843f8911c9dc58c8d
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 20:05:52 +0545

    string arrangement

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function myJoin(array, seperator){
+    if (seperator === undefined ){
+        seperator = ",";
+    }
+    let finalString = '';
+
+    for(let i=0; i< array.length ; i++){
+
+        let element = array[i]
+
+        if(i!==0){
+            finalString += seperator;
+        }
+        if (element !== null  && element !== undefined){
+        
+            finalString += element;
+        }
+        
+    }
+    return finalString;
+}
