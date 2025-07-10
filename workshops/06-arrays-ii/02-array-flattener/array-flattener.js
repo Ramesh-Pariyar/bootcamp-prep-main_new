f890208629d1d05d4a8f37f66011062bc4eeb2ac
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 17:05:59 +0545

    flattering of array

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,24 @@
-// YOUR CODE BELOW
+// Define a function, `arrayFlattener`, that accepts a two-dimensional array as an
+// argument.
+
+// `arrayFlattener` should return a new, one-dimensional array.
+
+// ```javascript
+// arrayFlattener([1,[2, 3], 4]); // => [1, 2, 3, 4]
+
+function arrayFlattener (twoDimArray){
+    let newArray = [];
+
+    for (let i = 0 ; i < twoDimArray.length ; i++){
+        if(Array.isArray(twoDimArray[i])){
+            for(let j = 0; j< twoDimArray[i].length; j++ ){
+                newArray.push(twoDimArray[i][j])
+                
+            }
+        }
+        else{
+            newArray.push(twoDimArray[i]);
+        }
+    }
+    return newArray;    
+}
