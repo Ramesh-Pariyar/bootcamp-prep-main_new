57143262587762d36a956c804ae70fcb4df8ea1b
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 12:24:24 +0545

    Implement myLastIndexOf function to find the last index of a value in an array

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,15 @@
-// YOUR CODE BELOW
+//Here is my code
+function myLastIndexOf(array,  searchValue, startIdx = 0){
+    let newArray = array.slice(startIdx)
+    if (searchValue === undefined){
+        return -1;  
+    }
+    
+    for(let i = newArray.length-1; i >= 0 ; i--){
+        
+        if (array[i] === searchValue){
+            return i;
+        }
+    }
+    return -1;
+}
