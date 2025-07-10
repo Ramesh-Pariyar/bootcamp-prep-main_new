f0580ec0f68bbe85ddad924fbd6fbb3f4b7306e1
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 11:32:15 +0545

    Implement myIncludes function to check for the presence of a value in an array

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,11 @@
-// YOUR CODE BELOW
+//My code is here
+function myIncludes(Arr,searchValue){
+
+    for (i = 0; i < Arr.length ; i++){
+        
+        if (Arr[i] === searchValue){
+            return true;
+        }
+    }
+    return false;
+}
