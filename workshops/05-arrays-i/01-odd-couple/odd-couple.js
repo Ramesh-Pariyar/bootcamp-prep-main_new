26feda52f9418674addd30ad3ab6e2ab042fafcc
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 11:25:09 +0545

    Implement oddCouple function to extract the first two odd numbers from an array

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,13 @@
-// YOUR CODE BELOW
+//My code is here
+function oddCouple(Arr){
+    let newArr = [];
+    for (i = 0; i<Arr.length; i++){
+        if (Arr[i]%2){
+            newArr.push(Arr[i])
+        }
+        if (newArr.length ===2){
+            break;
+        }
+    }
+    return newArr;
+}
