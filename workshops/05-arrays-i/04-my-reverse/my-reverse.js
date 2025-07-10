0adebcd929e7d26571ecc967fa9ad8df1d103990
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 13:07:36 +0545

    Implement myReverse function to reverse an array

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,9 @@
-// YOUR CODE BELOW
+//here is my code
+function myReverse (array){
+
+    let newArray = [];
+    for(i = array.length-1 ; i>=0; i--){
+        newArray.push(array[i])
+    }
+    return newArray;
+}
