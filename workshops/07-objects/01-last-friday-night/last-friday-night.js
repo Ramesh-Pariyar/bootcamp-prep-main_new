eb5040663e755a8f82c03661fcd60be8539de5a6
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 21:33:25 +0545

    summming total

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function lastFridayNight (transections){
+    let totalAmount = 0;
+
+    for ( i = 0 ; i< transections.length; i++){
+
+        let individualTransection = transections[i];
+        
+        totalAmount += individualTransection.amount;
+    }
+    return totalAmount;
+}
