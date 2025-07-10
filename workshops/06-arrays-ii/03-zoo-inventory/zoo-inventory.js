c3aac070453952dfb13f5b64efbd45a4ccc2bbdc
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 17:55:28 +0545

    zoo-inventory

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,30 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// Define a function, `zooInventory`, that accepts a multi-dimensional array of
+// animal facts.
+
+// `zooInventory` should return a new, flat array. Each element in the new array
+// should be a sentence about each of the animals in the zoo.
+
+// ```javascript
+// let myZoo = [
+//   ['King Kong', ['gorilla', 42]],
+//   ['Nemo', ['fish', 5]],
+//   ['Punxsutawney Phil', ['groundhog', 11]]
+// ];
+
+// // zooInventory(myZoo);
+
+function zooInventory(array){
+    let finalSentence = []
+
+    for(i = 0 ; i < array.length ; i++){
+
+        let animal = array[i][0];
+        let species = array[i][1][0]
+        let age = array[i][1][1]
+        let indSentence = `${animal} the ${species} is ${age}.`
+
+        finalSentence.push(indSentence)
+    }
+    return finalSentence;
+}
