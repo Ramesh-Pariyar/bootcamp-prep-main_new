8718694808f3e7e2dbb088e7804ea339fc8dc94e
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-10 18:36:41 +0545

    removed the columns

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function removeColumns(originalGrid,numColumns){
+    let newArray = [];
+
+    for (i = 0 ; i < originalGrid.length ; i ++){
+
+        let trimmedOriginalGrid = originalGrid[i];
+
+        let row = trimmedOriginalGrid.slice(0 , trimmedOriginalGrid.length-numColumns)
+
+        newArray.push(row)
+    }
+    return newArray;
+}
