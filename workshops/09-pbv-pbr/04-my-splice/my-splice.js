3a04c8c30884465e5f59b2d2bfd0b0455ce2da9c
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-11 20:19:48 +0545

    mutation of array

--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,1 +1,47 @@
-// YOUR CODE BELOW
+
+// Write a 'mySplice' function that mirrors the behavior of JavaScript's .splice()
+// array method. However, mySplice should accept the array to operate on as an
+// argument, rather than being invoked as a method on that array.
+
+// mySplice only needs to take one element to add to the array (the .splice method
+// can actually take any number of values to add).
+
+// Do not use .splice in your function.
+
+function mySplice (array, startIdx = 0, deleteStep=0, value ){ // assignint defualt values
+
+    let remainingArr1 = [];
+
+    let deletedArr=[];
+    
+    for (let i =  0 ; i < startIdx ; i++){
+        
+       remainingArr1.push(array[i])
+
+    }
+    if (value !== undefined){
+        remainingArr1.push(value)
+    }
+
+    for (let i = startIdx; i <startIdx+deleteStep; i++){
+        
+        deletedArr.push(array[i])
+    }
+
+    for (let i = startIdx + deleteStep ; i< array.length; i++){
+        remainingArr1.push(array[i])
+    }
+    array.length = 0;
+    for (let i = 0; i < remainingArr1.length ; i++){
+        array.push(remainingArr1[i])
+    }
+    
+    return deletedArr;
+
+}
+
+let myArray = [1, 2, 3];
+
+mySplice(myArray, 1, 1, 'apples') // => [2]
+
+console.log(myArray)    // [1,'apples', 3]
--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,14 @@
-// YOUR CODE BELOW
+// here is my code
+function reverseArray (array){
+    const rev = [];
+
+    for(let i = array.length-1 ; i >= 0 ; i--){
+        rev.push(array[i])
+    }
+    array.length = 0
+
+    for(let j = 0; j< rev.length; j++){
+        array.push(rev[j]);
+    }
+    return array;
+}
