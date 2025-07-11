b21b22a5074fabd8f50027b5cc925c20b4f8ba05
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-11 13:25:33 +0545

    adding new content to the original array

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,34 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+
+// '
+// Write a function, 'veryOddMutant'. The function accepts an array of numbers.
+// The function should mutate the given array by replacing every even number in the
+// array with the string 'normie'.
+
+// 'veryOddMutant' should return a count of the number of even numbers it replaced.
+
+// ```javascript
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let countRemoved = veryOddMutant(allTheNums);
+
+// console.log('allTheNums:', allTheNums);
+// // [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
+
+// console.log('countRemoved:', countRemoved);
+// // 4
+// ```
+
+function veryOddMutant (array){
+
+    let count = 0;
+
+    for( i = 0; i < array.length ; i++){
+
+        if ( array[i] % 2 === 0){
+            array[i] = "normie";
+            count++;
+        }
+    }
+    return count++;
+
+}
