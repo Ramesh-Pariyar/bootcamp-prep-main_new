6ea8881905d8dfbda03846ee8b7a56e15a5dc57c
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-11 14:08:12 +0545

    clone concept between parents and offspring

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,34 @@
-// YOUR CODE BELOW
+
+
+// Write a function `cloneMachine`, that, given an animal (represented as an object),
+// returns a clone of the original animal.
+
+// The name of the clone should be the name of its parent, concatenated with the word
+// 'Clone'.
+
+// `cloneMachine` should also push the name of the clone to the parent's offspring
+// array.
+
+// ```javascript
+// let dolly = {
+//   name: 'Dolly',
+//   species: 'sheep',
+//   offspring: []
+// }
+
+// let dollyClone = cloneMachine(dolly);
+
+// console.log(dollyClone) // {name: 'DollyClone', species: 'sheep', offspring: []}
+// console.log(dolly) // {name: 'Dolly', species: 'sheep', offspring: ['DollyClone']}
+// ```
+
+function cloneMachine (parent){
+
+    let child ={
+        name : parent.name+"Clone",
+        species : parent.species,
+        offspring : []
+    };
+    parent.offspring.push(child.name)
+    return child;
+}
