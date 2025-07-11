3818faebb36921c1148df43cff5a4d1a854a6513
Ramesh Pariyar <pariyar0412837i@gmail.com>
2025-07-11 22:00:52 +0545

    deep copy

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,29 @@
 // YOUR CODE BELOW
+function deeperCopy(array){
+
+    let copyarray = []
+
+    for (let i = 0; i < array.length ; i++){
+        
+        let element = array[i];
+
+        if(Array.isArray(element)){
+
+            let newEle = []
+
+            for ( let j = 0; j < element.length ; j ++){
+
+                newEle.push(element[j])
+            }
+
+            copyarray.push(newEle)
+
+        }
+        else{
+            copyarray.push(element)
+        }
+    }
+    return copyarray
+
+}
+console.log(deeperCopy([1,2,4,[1,2,],2,3]))
